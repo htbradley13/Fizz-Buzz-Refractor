2901e48ae364c6273e413bd4e3c5edafed85c46b
htbradley13 <htbradley13@gmail.com>
2016-08-15 22:00:16 -0400

    save

--- app.js
+++ app.js
@@ -3,30 +3,34 @@ $(document).ready(function() {
 	$("#Number-Selection").submit(function(event) {
 		//Since we aren't submitting to a server
 		event.preventDefault();
-		var numEntry = parseInt($('Number-entry').val())
 
-	if (numEntry % 3 === 0 && numEntry % 5 === 0) {
-			$('.Results').append('<p>' + 'Fizz Buzz' + '</p>' +'<br>');
+		//This is the number the user picks
+		var numEntry = parseInt($('#Number-entry').val());
+
+	//for (var gameStart = 1; gameStart <= numEntry; gameStart++) {
+
+	for (var numEntry; numEntry <= 100; numEntry++) {
+		
+		if (numEntry % 3 === 0 && numEntry % 5 === 0) {
+				$('.Results').append('<p>' + 'Fizz Buzz' + '</p>' +'<br>');
+			}
+		else if(numEntry % 3 === 0) {
+				$('.Results').append('<p>' + 'Fizz' + '</p>' +'<br>');
 		}
-	else if(numEntry % 3 === 0 && numEntry % 5 != 0) {
-			$('.Results').append('<p>' + 'Fizz' + '</p>' +'<br>');
-	}
-	else if(numEntry % 5 === 0 && numEntry % 3 != 0) {
-			$('.Results').append('<p>' + 'Buzz' + '</p>' +'<br>');
-	}
-	else {
-			$('.Results').append('<p>' + '$("#Number-entry").val()' + '</p>' +'<br>');
+		else if(numEntry % 5 === 0) {
+				$('.Results').append('<p>' + 'Buzz' + '</p>' +'<br>');
 		}
+		else {
+				$('.Results').append('<p>' + numEntry + '</p>' +'<br>');
+			}
+	}
 
-	};  
+		$(this)[0].reset();
 
-	$(this)[0].reset();
+	});  
 
 });
 
-});
 
-
-
-
+//$("#Number-entry").val()
 
